5f7b639efc330f21559db1545b5e5620d39a7614
Shyambarua <djdemoxit2003@gmail.com>
2026-04-02 21:40:45 +0530

    update

---
+++ User.Api/Models/AddClassModel.cs
@@ -0,0 +1,38 @@
+﻿using User.Api.DbEntities;
+
+namespace User.Api.Models
+{
+    public class AddClass
+    {
+        public int Id { get; set; }
+
+        public string ClassName { get; set; } = null!;
+
+        public string Section { get; set; } = null!;
+
+        public int ClassTeacherId { get; set; }
+
+        public string RoomNumber { get; set; } = null!;
+
+        public int Capacity { get; set; }
+
+        public int AcademicYear { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public string? Subjects { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        // Navigation Property (Foreign Key)
+        public Teacher? ClassTeacher { get; set; }
+    }
+}
